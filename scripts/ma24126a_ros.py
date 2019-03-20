b4c68b2e301c2f3e19c372ef237a9fe54ddb860e
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-03-20 21:51:17 +0900

    add ma24126a

---
+++ scripts/ma24126a_ros.py
@@ -0,0 +1,196 @@
+#! /usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float64
+from std_msgs.msg import Int32
+from std_msgs.msg import String
+import serial
+import time
+import sys,os
+
+sys.path.append("/home/amigos/ros/src")
+import threading
+import pymeasure
+
+
+class ma24126a_controller(object):
+    def __init__(self):
+
+        self.pm = pymeasure.usbpm.open("ma24126a")
+        self.rate = rospy.get_param("~rate")
+#flag
+        self.start_flag = 0
+        self.zero_set_flag = 0
+        self.close_flag = 0
+        self.power_flag = 0
+        self.avemode_flag = 0
+        self.avetyp_flag = 0
+        self.capt_flag = 0
+        self.mode_flag = 0
+
+
+#pubsub
+        rospy.Subscriber("ma24126a_start_cmd", Float64, self.start_switch)
+        rospy.Subscriber("ma24126a_zero_set_cmd", Float64, self.zero_set_switch)
+        rospy.Subscriber("ma24126a_close_cmd", Float64, self.close_switch)
+        rospy.Subscriber("ma24126a_power_cmd", Float64, self.power_switch)
+        rospy.Subscriber("ma24126a_avemode_cmd", Float64, self.avemode_switch)
+        rospy.Subscriber("ma24126a_avetyp_cmd", Float64, self.avetyp_switch)
+        rospy.Subscriber("ma24126a_capt_cmd", Float64, self.capt_switch, callback_ags=1)
+        rospy.Subscriber("ma24126a_avemode_cmd", Float64, self.avemode_switch, callback_ags=1)
+
+        self.pub_power = rospy.Publisher("ma24126a_power", Float64, queue_size = 1)
+
+
+
+#flag
+    def start_switch(self,q):
+        self.start_flag = q.data
+        return
+
+    def zero_set_switch(self,q):
+        self.zero_set_flag = q.data
+        return
+
+    def close_switch(self,q):
+        self.close_flag = q.data
+        return
+
+    def power_switch(self,q):
+        self.power_flag = q.data
+        return
+
+    def capt_switch(self,q):
+        self.capt = q.data
+        self.capt_flag = 1
+        return
+
+    def avemode_switch(self,q):
+        self.avemode = q.data
+        self.avemode_flag = 1
+        return
+
+    def avetyp_switch(self,q):
+        self.avetyp = q.data
+        self.avetyp_flag = 1
+        return
+
+    def mode_switch(self,q,flag):
+        self.mode = q.data
+        self.mode_flag = flag
+        return
+
+#main methond
+
+    def start(self):
+        while not rospy.is_shutdown():
+            if self.start_flag == 0:
+                continue
+
+            self.pm.start()
+
+            self.start_flag = 0
+            continue
+
+    def zero_set(self):
+        while not rospy.is_shutdown():
+            if self.zero_set_flag == 0:
+                continue
+
+            self.pm.zero_set()
+
+            self.zero_set_flag = 0
+            continue
+
+    def power(self):
+        while not rospy.is_shutdown():
+            if self.power_flag == 0:
+                continue
+
+            while self.power_flag == 1:
+                msg = Float64()
+                ret = self.pm.power()
+                try:
+                    msg.data = float(ret)
+                    self.pub_power.publish(msg)
+                except:
+                    pass
+                continue
+            continue
+
+
+    def close(self):
+        while not rospy.is_shutdown():
+            if self.close_flag == 0:
+                continue
+            self.pm.close()
+            self.close_flag = 0
+            continue
+
+    def change_capt(self):
+        while not rospy.is_shutdown():
+            if self.capt_flag == 0:
+                continue
+
+            self.pm.change_capt(self.capt)
+
+            self.capt_flag = 0
+            continue
+
+    def change_avemode(self):
+        while not rospy.is_shutdown():
+            if self.avemode_flag == 0:
+                continue
+
+            self.pm.change_avemode(self.avemode)
+
+            self.avemode_flag = 0
+            continue
+
+    def change_avetyp(self):
+        while not rospy.is_shutdown():
+            if self.avetyp_flag == 0:
+                continue
+
+            self.pm.change_avetyp(self.avetyp)
+
+            self.avetyp_flag = 0
+            continue
+
+    def change_mode(self):
+        while not rospy.is_shutdown():
+            if self.mode_flag == 0:
+                continue
+
+            self.pm.change_mode(self.mode)
+
+            self.mode_flag = 0
+            continue
+
+#thread
+
+    def start_thread(self):
+        th1 = threading.Thread(target=self.power)
+        th1.setDaemon(True)
+        th1.start()
+        th2 = threading.Thread(target=self.change_capt)
+        th2.setDaemon(True)
+        th2.start()
+        th3 = threading.Thread(target=self.change_avemode)
+        th3.setDaemon(True)
+        th3.start()
+        th4 = threading.Thread(target=self.zero_set)
+        th4.setDaemon(True)
+        th4.start()
+        th5 = threading.Thread(target=self.close)
+        th5.setDaemon(True)
+        th5.start()
+        th6 = threading.Thread(target=self.change_avetyp)
+        th6.setDaemon(True)
+        th6.start()
+
+if __name__ == "__main__" :
+    rospy.init_node("ma24126a")
+    ctrl = ma24126a_controller()
+    ctrl.start_thread()
+    rospy.spin()
