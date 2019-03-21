806d4c85a90956af25975361aedeec3bb836b01b
kotakayoshi <kousakaitatsu@kousakaitatsunoMacBook-Air.local>
2019-03-21 18:39:55 +0900

    for 1p85

--- scripts/p_att.py
+++ scripts/p_att.py
@@ -4,13 +4,15 @@ from std_msgs.msg import Int32
 
 
 class p_att_driver(object):
-    def __init__(self, self.ip='192.168.100.28', self.port=6001):
+    def __init__(self, ip='192.168.100.28', port=6001):
 
         self.pub_value = rospy.Publisher("/set_p_att1", Int32, queue_size=1)
         self.pub_value = rospy.Publisher("/set_p_att2", Int32, queue_size=1)
         self.pub_state1 = rospy.Publisher("/get_p_att1", Int32, queue_size=1)
         self.pub_state2 = rospy.Publisher("/get_p_att2", Int32, queue_size=1)
         
+        self.ip = ip
+        self.port = port
         com = pymeasure.ethernet(self.ip, self.port)
         IO = pymeasure.SENA.adio(com)
 
