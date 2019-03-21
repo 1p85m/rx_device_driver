4122c88c61060bef99988c2e26a71cc8f4ef0f7d
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-03-21 16:31:50 +0900

    test

--- scripts/ma24126a_ros.py
+++ scripts/ma24126a_ros.py
@@ -128,11 +128,10 @@ class ma24126a_controller(object):
     """
 
     def power(self):
+        msg = Float64()
         while not rospy.is_shutdown():
 
             ret = self.pm.power()
-
-            msg = Float64()
             msg.data = float(ret)
             self.pub_power.publish(msg)
 
