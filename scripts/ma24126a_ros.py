3824dc11bee84dc4bbb06b74c13f69f5e6ce2e8a
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-03-21 14:20:27 +0900

    test

--- scripts/ma24126a_ros.py
+++ scripts/ma24126a_ros.py
@@ -22,7 +22,7 @@ class ma24126a_controller(object):
         self.start_flag = 0
         self.zero_set_flag = 0
         self.close_flag = 0
-        self.power_flag = 1
+        self.power_flag = 0
         self.avemode_flag = 0
         self.avetyp_flag = 0
         self.capt_flag = 0
@@ -86,8 +86,7 @@ class ma24126a_controller(object):
         while not rospy.is_shutdown():
             if self.start_flag == 0:
                 continue
-            self.power_flag == 0
-            time.sleep(0.1)
+
             self.pm.start()
             time.sleep(0.1)
             self.start_flag = 0
