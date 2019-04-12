5c4637f6ddcf332f5119f96832391b816bbab48e
masui <masui@ogawalabnoMacBook-Pro.local>
2019-04-12 11:35:21 +0900

    add

--- scripts/tpg261_vaccume_monitor.py
+++ scripts/tpg261_vaccume_monitor.py
@@ -1,7 +1,7 @@
 import rospy, os, sys, time, serial, threading
 from std_msgs.msg import String
 
-
+from pymeasure import *
 class tpg261_driver(object):
     def __init__(self):
 
@@ -9,15 +9,19 @@ class tpg261_driver(object):
 
         self.tpg261 = serial.Serial("/dev/ttyUSB1",timeout=1)
 
+        self.query_pres = pressure()
+
+        self.query_bothpres = pressure()
+
+        self.query_gauge = gauge()
+
+        self.channel_dis = display()
+
+        self.error_moni = error()
+
     def query_pressure(self):
         while not rospy.is_shutdown():
-            self.tpg261.write(b"PR1 \r\n")
-            time.sleep(0.3)
-            self.tpg261.write(b"\x05")
-            time.sleep(0.3)
-            raw = self.tpg261.readline()
-            status = raw[0:1]
-            pressure = str(raw[2:13])
+            self.query_pres.pressure_device()
             if raw == b'\x06\r\n':
                 continue
             else:
@@ -31,6 +35,130 @@ class tpg261_driver(object):
                       self.pub_p.publish(msg)
                  else:
                       pass
+                  
+    def query_bothpressure(self):
+        while not rospy.is_shutdown():
+            self.query_bothpres.pressure_both()
+            if raw == b'\x06\r\n':
+                continue
+            else:
+                 if status1 == b'2' or status2 == b'2':
+                      msg = String()
+                      msg.data = Overrange
+                      self.pub_p.publish(msg)
+                 elif status1 == b'0' or status2 == b'0':
+                      msg = String()
+                      msg.data = pressure
+                      self.pub_p.publish(msg)
+                 else:
+                      pass
+
+
+
+    def check_gauge(self):
+            self.query_gauge.gauge_check()
+
+                 if status1 == b'0':
+                      msg = String()
+                      msg.data = cannot be changed
+                      self.pub_p.publish(msg)
+                 elif status1 == b'1':
+                      msg = String()
+                      msg.data = turned off
+                      self.pub_p.publish(msg)
+                 elif status1 == b'2':
+                            msg = String()
+                            msg.data = turned on
+                            self.pub_p.publish(msg)
+                 else:
+                      pass
+
+                   if status2 == b'0':
+                        msg = String()
+                        msg.data = cannot be changed
+                        self.pub_p.publish(msg)
+                   elif status2 == b'1':
+                        msg = String()
+                        msg.data = turned off
+                        self.pub_p.publish(msg)
+                   elif status2 == b'2':
+                              msg = String()
+                              msg.data = turned on
+                              self.pub_p.publish(msg)
+                   else:
+                        pass
+
+
+    def turn_gauge(self):
+            self.query_gauge.gauge_change(gague1,gague2)
+
+                 if status1 == b'0':
+                      msg = String()
+                      msg.data = cannot be changed
+                      self.pub_p.publish(msg)
+                 elif status1 == b'1':
+                      msg = String()
+                      msg.data = turned off
+                      self.pub_p.publish(msg)
+                 elif status1 == b'2':
+                      msg = String()
+                      msg.data = turned on
+                      self.pub_p.publish(msg)
+                 else:
+                      pass
+
+                   if status2 == b'0':
+                        msg = String()
+                        msg.data = cannot be changed
+                        self.pub_p.publish(msg)
+                   elif status2 == b'1':
+                        msg = String()
+                        msg.data = turned off
+                        self.pub_p.publish(msg)
+                   elif status2 == b'2':
+                        msg = String()
+                        msg.data = turned on
+                        self.pub_p.publish(msg)
+                   else:
+                        pass
+
+    def display_gauge1(self):
+        self.channel_dis.change_gague1()
+        msg = String()
+        msg.data = gague1
+        self.pub_p.publish(msg)
+
+    def display_gauge2(self):
+        self.channel_dis.change_gague2()
+        msg = String()
+        msg.data = gague2
+        self.pub_p.publish(msg)
+
+    def error_status(self):
+        self.error_moni.query_error()
+                   if status == b'0000':
+                        msg = String()
+                        msg.data = No error
+                        self.pub_p.publish(msg)
+                   elif status == b'1000':
+                        msg = String()
+                        msg.data = Controller Error see display
+                        self.pub_p.publish(msg)
+                   elif status == b'0100':
+                        msg = String()
+                        msg.data = No hardware
+                        self.pub_p.publish(msg)
+                   elif status == b'0010':
+                        msg = String()
+                        msg.data = Inadmissible parameter
+                        self.pub_p.publish(msg)
+                   elif status == b'0001':
+                        msg = String()
+                        msg.data = Syntax Error
+                        self.pub_p.publish(msg)
+                   else:
+                        pass
+
 
 if __name__ == "__main__" :
     rospy.init_node("tpg261")
@@ -42,4 +170,3 @@ if __name__ == "__main__" :
 
 #2019
 #written by T.Takashima
-
