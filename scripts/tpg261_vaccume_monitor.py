487b753937c201047ae046c623b2d5faea218392
masui <masui@ogawalabnoMacBook-Pro.local>
2019-04-17 16:49:36 +0900

    add

--- scripts/tpg261_vaccume_monitor.py
+++ scripts/tpg261_vaccume_monitor.py
@@ -2,34 +2,38 @@
 
 import rospy, os, sys, time, serial, threading
 from std_msgs.msg import String
+from std_msgs.msg import Float64
 
 from pymeasure_tpg261 import tpg261
 
 class tpg261_driver(object):
     def __init__(self):
 
-        self.pub_p = rospy.Publisher("/tpg_pressure", String, queue_size=1)
+        self.pub_er = rospy.Publisher("/tpg_error", String, queue_size=1)
+        self.pub_p = rospy.Publisher("/tpg_pressure", Float64, queue_size=1)
 
         self.dev = tpg261.device()
 
     def query_pressure(self):
         while not rospy.is_shutdown():
-            self.dev.pressure_device()
+            self.dev.pressure()
+            self.dev.pressure_error()
             self.b = self.dev.check()
-            status = self.dev.pressure_device()
-            print("akann1")
-            print(self.b)
+            pressure = self.dev.pressure()
+            status = self.dev.pressure_error()
             if self.b == 0:
+                pressure = tpg261.pressure_device()
+                self.pub_p.publish(pressure)
                 continue
             else:
                  if status == b'2':
                       msg = String()
                       msg.data = Overrange
-                      self.pub_p.publish(msg)
+                      self.pub_er.publish(msg)
                  elif status == b'0':
                       msg = String()
                       msg.data = pressure
-                      self.pub_p.publish(msg)
+                      self.pub_er.publish(msg)
                  else:
                      print("akann2")
                      pass
