77a5eb9ec666ff7e91f988c650f6abf522aa9dd2
masui <masui@ogawalabnoMacBook-Pro.local>
2019-04-18 17:55:36 +0900

    add

--- scripts/tpg261_vaccume_monitor.py
+++ scripts/tpg261_vaccume_monitor.py
@@ -23,7 +23,7 @@ class tpg261_driver(object):
             if self.b == 0:
                 pressure = self.dev.pressure()
                 print(pressure)
-                pressure1 = pressure.lstrip()
+                pressure1 = pressure.strip()
                 print(pressure1)
                 pres = float(pressure)
                 self.pub_p.publish(pres)
