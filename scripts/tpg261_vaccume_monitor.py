21fbc92b8b82b2636316b66e01fbbbcaeb70fa77
masui <masui@ogawalabnoMacBook-Pro.local>
2019-05-03 15:01:10 +0900

    add

--- scripts/tpg261_vaccume_monitor.py
+++ scripts/tpg261_vaccume_monitor.py
@@ -19,7 +19,7 @@ class tpg261_driver(object):
 #flag
         self.pres_flag = 2
         '''
-        0:cheks gague
+        0:check gague
         1:pressure
         2:unit
 
@@ -294,10 +294,10 @@ class tpg261_driver(object):
 if __name__ == "__main__" :
     rospy.init_node("tpg261")
     tpg = tpg261_driver()
-    thread_tpg_pres = threading.Thread(target=tpg.query_pressure)
-    thread_tpg_pres.start()
-    thread_tpg_gauge = threading.Thread(target=tpg.check_gauge)
-    thread_tpg_gauge.start()
+    #thread_tpg_pres = threading.Thread(target=tpg.query_pressure)
+    #thread_tpg_pres.start()
+    #thread_tpg_gauge = threading.Thread(target=tpg.check_gauge)
+    #thread_tpg_gauge.start()
     thread_tpg_unit = threading.Thread(target=tpg.change_unit)
     thread_tpg_unit.start()
 
